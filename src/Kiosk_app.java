e474bd72dbacbe56d4767942791846f4e6d6bbc2
Kyungju <wwmix@naver.com>
2023-03-16 23:49:17 +0900

    init 메소드 ㅈ초안 작성

--- src/Kiosk_app.java
+++ src/Kiosk_app.java
@@ -1,2 +1,9 @@
 public class Kiosk_app {
+
+    public void init() {
+        System.out.println("[안내] 안녕하세요. 김밥천국에 오신 것을 환영합니다.");
+        System.out.println("-".repeat(50));
+
+        System.out.println("원하시는 메뉴의 번호를 입력하여 주세요.");
+    }
 }
