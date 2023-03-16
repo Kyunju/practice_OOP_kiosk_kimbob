ba6914d7231aaaa3619e656191204b47c604f0a2
Kyungju <wwmix@naver.com>
2023-03-16 23:45:22 +0900

    Product 클래스 뼈대 완성

---
+++ src/Product.java
@@ -0,0 +1,27 @@
+public class Product {
+    private String name; // 상품명
+    private int price; // 가격
+    private int amount = 0; // 상품 갯수
+
+    public Product(String name, int price, int amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
