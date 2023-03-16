d60e2ac3c4eb9bab33da1cfdc2894b1dae3dfd84
Kyungju <wwmix@naver.com>
2023-03-16 23:46:02 +0900

    ProductRepository  클래스 기본 뼈대 완성

--- src/ProductRepository.java
+++ src/ProductRepository.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class ProductRepository {
+public class ProductRepository {
+    private Product[] menu = new Product[]{
+            new Product("김밥", 1000, 99),
+            new Product("계란 김밥", 1500, 99),
+            new Product("충무 김밥", 1000, 99),
+            new Product("떡볶이", 2000, 99)
+    };
+
+    public Product[] getMenu() {
+        return menu;
+    }
 }
